23f2c0efeb438d1635daaa2318bfc9cffd8c61d6
draganet <3417237276@qq.com>
2021-10-02 09:48:13 +0800

    update

--- src/test.js
+++ src/test.js
@@ -1,78 +1,65 @@
-import string from "./css"
+import string from "./css.js"
 
-let n = 1
-// demo.innerText = string.substring(0, n)
-// demo2.innerHTML = string.substring(0, n)
-// console.log(n)
-let time = 50
-const run = () => {
-    n += 1
-    if (n > string.length) {
-        window.clearInterval(id)
-        return
+const player = {
+    id: undefined,
+    n: 1,
+    time: 100,
+    ui: {
+        demo: document.querySelector('#demo'),
+        demo2: document.querySelector('#demo2')
+    },
+    init: () => {
+        player.ui.demo.innerText = string.substring(0, player.n)
+        player.ui.demo2.innerHTML = string.substring(0, player.n)
+        player.bindEvents()
+        player.play()
+    },
+    events: {
+        '#btnPause': 'pause',
+        '#btnPlay': 'play',
+        '#btnSlow': 'slow',
+        '#btnNormal': 'normal',
+        '#btnFast': 'fast'
+    },
+    bindEvents: () => {
+        for (let key in player.events) {
+            if (player.events.hasOwnProperty(key)) {
+                const value = player.events[key]
+                document.querySelector(key).onclick = player[value]
+            }
+        }
+    },
+    run: () => {
+        player.n += 1
+        if (player.n > string.length) {
+            window.clearInterval(player.id)
+            return
+        }
+        player.ui.demo.innerText = string.substring(0, player.n)
+        player.ui.demo2.innerHTML = string.substring(0, player.n)
+        player.ui.demo.scrollTop = player.ui.demo.scrollHeight
+    },
+    play: () => {
+        player.id = setInterval(player.run, player.time)
+    },
+    pause: () => {
+        window.clearInterval(player.id)
+    },
+    slow: () => {
+        player.pause()
+        player.time = 200
+        player.play()
+    },
+    normal: () => {
+        player.pause()
+        player.time = 80
+        player.play()
+    },
+    fast: () => {
+        player.pause()
+        player.time = 0
+        player.play()
     }
-    demo.innerText = string.substring(0, n)
-    demo2.innerHTML = string.substring(0, n)
-    demo.scrollTop = demo.scrollHeight
-    // console.log(string.substring(0, n))
-}
-let id = setInterval(() => {
-    run()
-}, time);
-
-btnPause.onclick = () => {
-    window.clearInterval(id)
 }
 
-btnPlay.onclick = () => {
-    id = setInterval(() => {
-        run()
-    },time)
-}
-
-
-btnSlow.onclick = () => {
-    window.clearInterval(id)
-    time = 150
-    id = setInterval(() => {
-        run()
-    },time)
-    console.log(time)
-}
-
-btnNormal.onclick = () =>{
-    window.clearInterval(id)
-    time = 50
-    id = setInterval(() =>{
-        run()
-    },time)
-    console.log(time)
-}
-
-btnFast.onclick = () => {
-    window.clearInterval(id)
-    time = 0
-    id = setInterval(() => {
-        run()
-    })
-    console.log(time)
-}
-
-
-
-dev
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+player.init()
